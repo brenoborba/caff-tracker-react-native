96e247b14ce8bb9f334d480883c527e463edb43f
Breno Bessa Borba Alcantara <a40541@alunos.ipb.pt>
2022-07-08 15:10:22 +0100

    Basic images added to assets folder

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Image source={'./assets/logo'} />
       <Text>Caff tracker</Text>
       <StatusBar style="auto" />
     </View>
@@ -13,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#79553e',
     alignItems: 'center',
     justifyContent: 'center',
   },
